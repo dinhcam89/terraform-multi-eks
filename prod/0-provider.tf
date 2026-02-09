37971895a93155d89ecd71fb2a6ba7bfa692d76a
dinhcam89 <dinhcam1512@gmail.com>
2026-02-09 10:09:38 +0700

    add state backend configuration

--- prod/0-provider.tf
+++ prod/0-provider.tf
@@ -9,6 +9,14 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "eks-infra-state"
+    key            = "prod/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "infra-state-db"
+    encrypt        = true
+  }
 }
 
 
--- staging/0-provider.tf
+++ staging/0-provider.tf
@@ -9,6 +9,14 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "eks-infra-state"
+    key            = "staging/terraform.tfstate"
+    region         = "ap-southeast-1"
+    dynamodb_table = "infra-state-db"
+    encrypt        = true
+  }
 }
 
 
